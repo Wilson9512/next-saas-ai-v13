21bc8711043e0f81fc62612483c56083870854a3
Wilson Chen <wilson.c@mydailylive.com>
2023-10-20 15:37:28 +0800

    remove test working

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
-import createMiddleware from "next-intl/middleware";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 
-const intlMiddleware = createMiddleware({
-  locales: ['en', 'zh'],
-  defaultLocale: 'en'
-});
-
 export default authMiddleware({
-  beforeAuth(request) {
-    return intlMiddleware(request);
-  },
   publicRoutes: ["/", "/en", "/zh", "/api/webhook", "/zh/resume", "/en/resume", "/resume"],
 });
 
